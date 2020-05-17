5d1437877fc4317c88d415504a3194ac825cf324
JamesBock <37028244+JamesBock@users.noreply.github.com>
2020-05-16 21:03:57 -0500

    Added txt for each test

---
+++ NLMDrugInteractionParser/MedicationInteractionList.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLMDrugInteractionParser
+{
+    public class MedicationInteractionList 
+    {
+        public List<Constituent> Constituents { get; set; }
+
+
+    }
+    public class Constituent
+    {       
+        public string Interactor { get; set; }
+        public string RxCUI { get; set; }
+
+    }
+}
--- NLMDrugInteractionParserTests/DrugInteractionParserTests.cs
+++ NLMDrugInteractionParserTests/DrugInteractionParserTests.cs
@@ -20,7 +20,7 @@ namespace NLMDrugInteractionParser.Tests
         [TestMethod()]
         public void ParseSingleDrugInteractionsTest()
         {
-            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParser\SingleDrugMuliIngredientString.txt");
+            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParserTests\TestJsonText\SingleDrugMuliIngredientString.txt");
             var jstring = reader.ReadToEnd();
             var parser = new SingleDrugInteractionParser();
             var interactions = parser.ParseDrugInteractions(jstring);
@@ -37,11 +37,12 @@ namespace NLMDrugInteractionParser.Tests
         [TestMethod()]
         public async Task ParseSingleDrugInteractionsAsyncTest()
         {
-            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParser\SingleDrugMuliIngredientString.txt");
+            //var interactions = _client.GetInteractions(jstring);
+
+            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParserTests\TestJsonText\SingleDrugMuliIngredientString.txt");
             var jstring = await reader.ReadToEndAsync();
             var parser = new SingleDrugInteractionParser();
             var interactions = await  parser.ParseDrugInteractionsAsync(jstring);
-            //var interactions = _client.GetInteractions(jstring);
 
             //Are medication with multiple ingredients being parsed correctly?
             Assert.AreEqual("acetaminophen 33.3 MG/ML / diphenhydramine hydrochloride 1.67 MG/ML [Tylenol PM] (1092374) is resolved to acetaminophen (161)", interactions.Select(o => o.Comment).FirstOrDefault());
@@ -56,11 +57,14 @@ namespace NLMDrugInteractionParser.Tests
         [TestMethod()]
         public void ParseDrugInteractionsTest()
         {            
-            var interactions = _client.GetInteractionList(rxCUIs);
-          
+            //var interactions = _client.GetInteractionList(rxCUIs);
 
-            //Are medication with multiple ingredients being parsed correctly?
-            Assert.AreEqual(29, interactions.Count);
+            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParserTests\TestJsonText\MultiDrugString.txt");
+            var jstring = reader.ReadToEnd();
+            var parser = new DrugInteractionParser();
+            var interactions = parser.ParseDrugInteractions(jstring);
+
+            
             //Test for JAMIA interaction inclusion. All DrugBank interactions have "N/A" as severity
             Assert.AreEqual(true, interactions
                                         .SelectMany(x => x.DrugInteractionDetails
@@ -77,16 +81,20 @@ namespace NLMDrugInteractionParser.Tests
             var first = interactions.Select(x => x.DrugInteractionDetails.First().Description).First();
             Assert.AreEqual("No Drug-Drug Interactions Found", first);  
         }
+       
         [TestMethod()]
         public async Task ParseDrugInteractionsAsyncTest()
         {
-            var response = _client.GetInteractionListAsync(rxCUIs);
-            
+            //var response = _client.GetInteractionListAsync(rxCUIs);
+            var reader = new StreamReader(@"C:\Users\James\source\repos\NLMDrugInteractionParserSolution\NLMDrugInteractionParserTests\TestJsonText\MultiDrugString.txt");
+            var jstring = await reader.ReadToEndAsync();
+            var parser = new DrugInteractionParser();
+            var response = parser.ParseDrugInteractionsAsync(jstring);
+            var interactions = await response;
 
             Assert.AreEqual(response.GetType(), typeof(Task<List<MedicationInteractionPair>>));
-            var interactions = await response;
             //Are medication with multiple ingredients being parsed correctly?
-            Assert.AreEqual(29, interactions.Count);
+            //Assert.AreEqual(29, interactions.Count);
             //Test for JAMIA interaction inclusion. All DrugBank interactions have "N/A" as severity
             Assert.AreEqual(true, interactions
                                         .SelectMany(x => x.DrugInteractionDetails
